6f8f8f7d7b672f9f771d3286ecfc024d1b5f83ea
Henrique Gravina <henrique@gravina.com.br>
2023-12-01 18:51:35 +0000

    Adicionando primeiro endpoint da api

--- tests/unit/calculadora.test.js
+++
@@ -1,9 +0,0 @@
-const calculadora = require("../models/calculadora.js");
-
-test("Deve retornar 4", () => {
-  expect(calculadora.somar(2, 2)).toBe(4);
-});
-
-test("Deve retornar 105", () => {
-  expect(calculadora.somar(5, 100)).toBe(105);
-});
