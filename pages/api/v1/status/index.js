f40a2f084f98a6e82ba8acf90c35a7faa253673f
Henrique Gravina <henrique@gravina.com.br>
2023-12-05 17:17:06 +0000

    Adicionado versao ao endpoint status e numero de conexoe e tryno acesso ao BD

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,34 @@
 import database from "infra/database.js";
+const os = require("os");
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  response.status(200).json({ chave: "Teste do cursopontodev que é bão" });
+  const updatedAt = new Date().toISOString();
+
+  const pgVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = pgVersion.rows[0].server_version;
+
+  const pgConnCount = await database.query(
+    "SELECT count(*) FROM pg_stat_activity;",
+  );
+  const databaseCurrenConnectionsValue = pgConnCount.rows[0].count;
+
+  const systemUptime = os.uptime();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        currntConnections: databaseCurrenConnectionsValue,
+      },
+    },
+    system: {
+      uptime: systemUptime,
+    },
+    process: {
+      uptime: process.uptime(),
+    },
+  });
 }
 
 export default status;
